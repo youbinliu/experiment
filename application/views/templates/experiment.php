0403f101af591aecd43d24bb4af0b2c17f33dca4
wfly214 <wfly214@gmail.com>
2013-04-12 18:38:16 +0800

    a little change for show

--- application/views/templates/experiment.php
+++ application/views/templates/experiment.php
@@ -22,7 +22,7 @@
     	  		    }
             endif; ?>
       </select>
-      <label>关键词<label class="label label-info">（每个关键词用空格分开，小于40个字符）</label></label>
+      <label>关键词<label class="label label-info">（每个关键词用","分开，小于40个字符）</label></label>
       <input type="text" <?php if(!empty($default['keywords'])) echo 'value='.$default['keywords']; ?> name="experiment_keyword" style="width: 40%;" maxlength="40">
       
       <label>实验描述</label>
